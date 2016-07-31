f227c263faa216fffe65ce770f0c100c9b156b32
zhengsl <zhengsl@ZHENGSL-PC>
2016-07-31 12:09:16 +0800

    assembly rename

--- Semantic.ChartCommon/Properties/AssemblyInfo.cs
+++ Semantic.ChartCommon/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Microsoft.Reporting.Windows.Common.Internal")]
-[assembly: AssemblyDescription("Microsoft.Reporting.Windows.Common.Internal")]
+[assembly: AssemblyTitle("Semantic.ChartCommon")]
+[assembly: AssemblyDescription("Semantic.ChartCommon")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Microsoft.Reporting.Windows.Common.Internal")]
+[assembly: AssemblyProduct("Semantic.ChartCommon")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.7.0")]
 [assembly: AssemblyFileVersion("1.0.7030.1021")]
